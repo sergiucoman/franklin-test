012055c3d4c89ff51475010f4d5f1397a74ac352
sergiucoman <sergiu.coman@gmail.com>
2023-02-24 10:27:22 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,31 @@
 
+function intoCards(main) {
+
+ main.querySelectorAll(".rows > div")
+ .forEach(column => {
+
+   // first level is made up of columns
+   column.className = "column"
+
+   let card = document.createElement("div");
+
+   // we need to create a card div
+   card.className = "card";
+
+   // move the picture
+   column.querySelectorAll("div > p > picture ").forEach(inner => {
+     card.appendChild(inner);
+   });
+
+   // move the text
+   column.querySelectorAll("div > p  ").forEach(inner => {
+     card.appendChild(inner);
+   });
+
+   column.appendChild(card)
+
+ });
+
+}
+
+intoCards(document.querySelector("main"));
